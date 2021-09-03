89dba35fd855aaa06eb0123ee8aa3ad9cbfab379
JosephSilva362 <josephsilva362@gmail.com>
2021-09-03 12:16:06 -0300

    4. Controller Final

--- src/main/java/joseph/digitalinnovation/live/controller/JornadaTrabalhoController.java
+++ src/main/java/joseph/digitalinnovation/live/controller/JornadaTrabalhoController.java
@@ -3,10 +3,11 @@ package joseph.digitalinnovation.live.controller;
 import joseph.digitalinnovation.live.model.JornadaTrabalho;
 import joseph.digitalinnovation.live.service.JornadaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/jornada")
@@ -17,5 +18,31 @@ public class JornadaTrabalhoController {
     @PostMapping
     public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
         return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    @PutMapping
+    public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaLIst(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/idJornada")
+    public ResponseEntity<JornadaTrabalho> getJornadaById(@PathVariable("idJornada") Long idJornada) throws Exception{
+        return ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Jornada nao encontrada!")));
+    }
+
+    @DeleteMapping("/{idJornada}")
+    public ResponseEntity deleteByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+        try {
+            jornadaService.deleteJornada(idJornada);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return (ResponseEntity<JornadaTrabalho>) ResponseEntity.ok();
+
     }
 }
--- src/main/java/joseph/digitalinnovation/live/service/JornadaService.java
+++ src/main/java/joseph/digitalinnovation/live/service/JornadaService.java
@@ -4,6 +4,9 @@ import joseph.digitalinnovation.live.model.JornadaTrabalho;
 import joseph.digitalinnovation.live.repository.JornadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.Optional;
+
 public class JornadaService {
 
     JornadaRepository jornadaRepository;
@@ -15,5 +18,21 @@ public class JornadaService {
 
     public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho) {
         return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public JornadaTrabalho updateJornada(JornadaTrabalho jornadaTrabalho) {
+        return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public List<JornadaTrabalho> findAll() {
+        return jornadaRepository.findAll();
+    }
+
+    public Optional<JornadaTrabalho> getById(Long idJornada){
+        return jornadaRepository.findById(idJornada);
+    }
+
+    public void deleteJornada(Long idJornada){
+        jornadaRepository.deleteById(idJornada);
     }
 }
